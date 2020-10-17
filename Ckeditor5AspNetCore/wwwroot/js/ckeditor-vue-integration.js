11d041d4c4fe6ab1d7d1b523fae705e749c1de8d
Amdadul Haque <nirab.25@gmail.com>
2020-10-17 22:13:00 +0600

    Folder rearranged

---
+++ Ckeditor5AspNetCore/wwwroot/js/ckeditor-vue-integration.js
@@ -0,0 +1,21 @@
+﻿Vue.use(CKEditor);
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        editor: ClassicEditor,
+        editorData: '<p>Content of the editor.</p>',
+        editorConfig: {
+            // The configuration of the editor.
+            extraPlugins: [this.MyCustomUploadAdapterPlugin]
+        }
+    },
+    methods: {
+        MyCustomUploadAdapterPlugin(editor) {
+            this.editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+                // Configure the URL to the upload script in your back-end here!
+                return new MyUploadAdapter(loader);
+            };
+        }
+    }
+});
