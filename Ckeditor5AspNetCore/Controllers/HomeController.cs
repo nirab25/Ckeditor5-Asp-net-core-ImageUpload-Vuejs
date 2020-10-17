bc7f5bfb38de5c33f7d24990514b59412e43fda4
Amdadul Haque <nirab.25@gmail.com>
2020-10-17 19:28:21 +0600

    Add files via upload

---
+++ Ckeditor5AspNetCore/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Ckeditor5AspNetCore.Models;
+using System.IO;
+
+namespace Ckeditor5AspNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("Home/UploadCKEditorImage")]
+        public JsonResult UploadCKEditorImage()
+        {
+            var files = Request.Form.Files;
+            if (files.Count == 0)
+            {
+                var rError = new
+                {
+                    uploaded = false,
+                    url = string.Empty
+                };
+                return Json(rError);
+            }
+
+            var formFile = files[0];
+            var upFileName = formFile.FileName;
+            
+            var fileName = Path.GetFileNameWithoutExtension(upFileName) + 
+                DateTime.Now.ToString("yyyyMMddHHmmss") + Path.GetExtension(upFileName);
+
+            var saveDir = @".\wwwroot\upload\";
+            var savePath = saveDir + fileName;
+            var previewPath = "/upload/" + fileName;
+
+            bool result = true;
+            try
+            {
+                if (!Directory.Exists(saveDir))
+                {
+                    Directory.CreateDirectory(saveDir);
+                }
+                using (FileStream fs = System.IO.File.Create(savePath))
+                {
+                    formFile.CopyTo(fs);
+                    fs.Flush();
+                }
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+            var rUpload = new
+            {
+                uploaded = result,
+                url = result ? previewPath : string.Empty
+            };
+            return Json(rUpload);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
